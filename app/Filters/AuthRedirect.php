429eabe7d2ff1fbef38049250a37a57e5f220306
RenePoprenak <poprenakrene@gmail.com>
2026-01-15 13:58:40 +0100

    feat(auth): add auth redirect filter for protected pages

---
+++ app/Filters/AuthRedirect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+final class AuthRedirect implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        $auth = service('auth');
+
+        if ($auth->loggedIn()) {
+            return;
+        }
+
+        // set redirect_url to current url
+        session()->set('redirect_url', current_url(true)->__toString());
+
+        // shield login page
+        return redirect()->to(site_url('login'));
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // now nothing
+    }
+}
