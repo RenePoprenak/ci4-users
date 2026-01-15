82cf22793aa9c8ab6f62f61c6ea6ecc03487d48a
RenePoprenak <poprenakrene@gmail.com>
2026-01-15 14:16:13 +0100

    i18n(auth): add slovak translations for authentication

---
+++ app/Language/sk/Auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    // Buttons / actions
+    'login'        => 'Prihlásiť sa',
+    'logout'       => 'Odhlásiť sa',
+
+    // Fields
+    'email'        => 'E-mail',
+    'password'     => 'Heslo',
+
+    // Titles / headings
+    'loginTitle'   => 'Prihlásenie',
+    'loginIntro'   => 'Prihláste sa do systému',
+
+    // Messages
+    'invalidLogin' => 'Nesprávny e-mail alebo heslo.',
+    'loggedIn'     => 'Úspešne prihlásený.',
+    'loggedOut'    => 'Úspešne odhlásený.',
+
+    // Validation / errors
+    'required'     => 'Pole {field} je povinné.',
+];
