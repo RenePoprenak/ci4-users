a65b5883deb9381809933679c63ff232f2d5aea2
RenePoprenak <poprenakrene@gmail.com>
2026-01-15 14:03:11 +0100

    routes: protect homepage and admin routes with auth redirect

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,18 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
+// protected homepage
+$routes->get('/', 'Home::index', ['filter' => 'auth.redirect']);
+
+// admin section (for now placeholder)
+$routes->group('admin', ['filter' => 'auth.redirect'], static function ($routes) {
+    $routes->get('/', 'Admin\Dashboard::index');
+});
+
+// Shield auth routes (/login, /logout, /register ...)
 service('auth')->routes($routes);
 
-$routes->get('users', 'UsersController::index', ['as' => 'users.index']);
-$routes->get('users/(:num)', 'UsersController::show/$1', ['as' => 'users.show']);
+// other routes
+$routes->get('users', 'UsersController::index', ['as' => 'users.index', 'filter' => 'auth.redirect']);
+$routes->get('users/(:num)', 'UsersController::show/$1', ['as' => 'users.show', 'filter' => 'auth.redirect']);
