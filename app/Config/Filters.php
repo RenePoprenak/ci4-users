6fe6dc4dab36d3b9eb37ef792d1f02aaf750ea68
RenePoprenak <poprenakrene@gmail.com>
2026-01-15 13:59:47 +0100

    config(filters): register auth redirect filter alias

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'auth.redirect' => \App\Filters\AuthRedirect::class,
     ];
 
     /**
